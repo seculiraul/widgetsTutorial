d361ef531aeeea10b6dd42b3d20c8726506adcbb
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-11 23:47:46 +0200

    S12L161

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+            params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: term
+            }
+        })
+        setResults(data.query.search);
+    }
+    if(term){
+        search();
+    }
+    }, [term])
+
+    const renederdResults = results.map(result => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                    className="ui button"
+                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div className="">
@@ -13,6 +54,9 @@ const Search = () => {
                     onChange={(e) => setTerm(e.target.value)}
                     className="input" />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renederdResults}
             </div>
         </div>
     )
