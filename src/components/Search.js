ac9d8bd509b0883149bc1594ef8229a67ebfdde8
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-12 22:22:28 +0200

    S12L169

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,25 +3,37 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
     useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId)
+        };
+    },[term])
+
+    useEffect(() => {
         const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+          const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
             params: {
-                action: 'query',
-                list: 'search',
-                origin: '*',
-                format: 'json',
-                srsearch: term
-            }
-        })
-        setResults(data.query.search);
-    }
-    if(term){
-        search();
-    }
-    }, [term])
+              action: 'query',
+              list: 'search',
+              origin: '*',
+              format: 'json',
+              srsearch: debouncedTerm,
+            },
+          });
+     
+          setResults(data.query.search);
+        };
+        if (debouncedTerm) {
+          search();
+        }
+      }, [debouncedTerm]);
 
     const renederdResults = results.map(result => {
         return (
