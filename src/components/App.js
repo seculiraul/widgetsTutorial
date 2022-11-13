f2f26f8454bcfbd6f0c8065bbea4bdd7f5203dc3
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-13 23:26:01 +0200

    S13L205

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Accordion from "./Accordion";
 //import Accordion from "./Accordion";
 //import Search from "./Search";
-//import Dropdown from "./Dropdown";
+import Dropdown from "./Dropdown";
 import Translate from './Translate';
+import Route from "./Route";
+import Search from "./Search";
+import Header from "./Header";
 
-/*const items = [
+const items = [
     {
         title: 'What is React?',
         content: 'React is a JS Framework'
@@ -17,13 +21,46 @@ import Translate from './Translate';
         title: 'How to use Resct?',
         content: 'You can use React by creating components'   
     }
-]*/
+]
+
+const options = [
+    {
+        label: 'Red',
+        value: 'red'
+    },
+    {
+        label: 'Green',
+        value: 'green'
+    },
+    {
+        label: 'Blue',
+        value: 'blue'
+    }
+]
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0])
     return (
     <div>
         <br />
-        <Translate />
+        <Header />
+        <Route path="/">
+            <Accordion items={items}/>
+        </Route>
+        <Route path="/list">
+            <Search />
+        </Route>
+        <Route path="/dropdown">
+            <Dropdown
+            label="Select a color"
+            onSelectedChange={setSelected}
+            options={options}
+            selected={selected}
+             />
+        </Route>
+        <Route path="/translate">
+            <Translate />
+        </Route>
     </div>
     )
 
